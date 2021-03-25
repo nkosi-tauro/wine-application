5f44af3ce0228f3b20fc652508afa739ad35de60
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-25 18:47:58 +0200

    add create form

--- client/src/views/CreateWine.vue
+++ client/src/views/CreateWine.vue
@@ -1,16 +1,201 @@
 <template>
   <div class="row justify-content-center">
     <div class="col-md-6">
-      Create
+      <h3 class="text-center">New Wines</h3>
+      <form @submit.prevent="CreateWine">
+        <div class="form-group">
+          <label>Title</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="title"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Country</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="country"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>region</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="region"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>lage</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="lage"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>SweetNess</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="sweetness"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Sugar Level</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="sugarlevel"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Type</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="wineType"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Color</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="wineColor"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Description</label>
+          <textarea
+            type="text"
+            class="form-control"
+            v-model="desc"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Alcohol Level</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="alcLevel"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Vintage</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="vintage"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Is the EAN valid?
+          <input
+            type="checkbox"
+            class="form-control"
+            v-model="validEan"
+            required
+          />
+          </label>
+        </div>
+
+        <div class="form-group">
+          <label>Acidity</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="acidity"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Winery</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="winery"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Grape</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="Grape"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <label>Appeallation</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="appellation"
+            required
+          />
+        </div>
+
+        <div class="form-group">
+          <button class="btn btn-danger btn-block">Create</button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+import {reactive, toRefs} from 'vue';
 export default {
-  name: 'Create',
+  name: "Create",
   setup() {
-    return {};
+    const state = reactive({
+      title: "",
+      country: "",
+      region: "",
+      lage: "",
+      sweatness: "",
+      sugarlevel: "",
+      wineType: "",
+      wineColor: "",
+      desc: "",
+      alcLevel: "",
+      vintage: "",
+      validEan: false,
+      acidity: "",
+      winery: "",
+      grape: "",
+      appellation: "",
+    })
+    return {...toRefs(state)};
   },
 };
 </script>
