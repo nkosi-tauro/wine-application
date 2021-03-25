45b8e24ab9b8ba558a7e66a068b3b81473498f6b
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-25 18:25:06 +0200

    add navigation

---
+++ client/src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav
+    class="navbar navbar-dark bg-primary justify-content-between flex-nowrap flex-row"
+  >
+    <div class="container">
+      <router-link class="nav-link pr-3" to="/"><a class="navbar-brand float-left">Wine Store</a></router-link>
+      <ul class="nav navbar-nav flex-row float-right">
+        <li class="nav-item">
+          <router-link class="nav-link pr-3" to="/create">New Wine</router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
