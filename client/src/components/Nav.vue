f5310d440b5c4faed83e2ddf63124b5ecc809210
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-25 18:27:36 +0200

    update nav

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -6,6 +6,9 @@
       <router-link class="nav-link pr-3" to="/"><a class="navbar-brand float-left">Wine Store</a></router-link>
       <ul class="nav navbar-nav flex-row float-right">
         <li class="nav-item">
+          <router-link class="nav-link pr-3" to="/">View Wines</router-link>
+        </li>
+        <li class="nav-item">
           <router-link class="nav-link pr-3" to="/create">New Wine</router-link>
         </li>
       </ul>
