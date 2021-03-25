b8a8ebafd60c92d765185d91ec981f85aa3b5c53
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-25 17:59:33 +0200

    add routes to express

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const mongoUri = `mongodb+srv://nkosi-tauro:${process.env.DB_PASS}@vuejwtauth.mpdvx.mongodb.net/wineapp?retryWrites=true&w=majority`
 const app = express()
+const routes = require('./routes/routes')
 const port = process.env.PORT || 3000
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }))
 app.use(cors())
-
+app.use('/api', routes)
 
 // mongoDB
 mongoose.connect(mongoUri, {
