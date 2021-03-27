a2830a00ef7c9ee0d249c8c642e242ac92c719c7
nkosi-tauro <nkosilati23@yahoo.com>
2021-03-27 16:10:18 +0200

    update docker proxy

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,6 +3,6 @@ const path = require('path')
 module.exports = {
   outputDir: path.resolve(__dirname, '../backend/public'),
   devServer: {
-    proxy: 'http://localhost:3000' || process.env.PORT
+    proxy: 'http://wine-backend:3000/' || process.env.PORT
   }
 }
